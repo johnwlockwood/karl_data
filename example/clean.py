de61100e5611afb214ae4f9b984db3ccb4d9a3af
John Lockwood <johnwlockwood@gmail.com>
2014-04-10 15:26:25 -0700

    Define an example csv data cleaner
    It changes all the fields of the input data file to title case.

--- example/clean.py
+++ example/clean.py
@@ -1,19 +1,64 @@
 import argparse
+from functools import partial
+from operator import methodcaller
+import os
+from karld.loadump import is_file_csv
+from karld.run_together import csv_file_to_file
+from karld.run_together import pool_run_files_to_files
+from karld.run_together import serial_run_files_to_files
 
 
-def run(in_dir='dirty_data', out_dir='clean_data'):
-    pass
+def titlize(data_items):
+    """
+    Applies title case to every field.
+
+    :param data_items: A sequence of unicode strings
+    """
+    return (map(methodcaller('title'), row) for row in data_items)
+
+
+def run(in_dir, out_dir, pool):
+    """
+
+    """
+    files_to_files_runner = serial_run_files_to_files
+
+    if pool:
+        print("multi-processing")
+        files_to_files_runner = pool_run_files_to_files
+
+    files_to_files_runner(
+        partial(csv_file_to_file,
+                titlize,
+                "",
+                out_dir),
+        in_dir, filter_func=is_file_csv)
 
 
 def main(*args):
+    """
+    Try it::
+
+        python clean.py
+
+    or::
+
+        python clean.py --in-dir split_data_ml/unsplit --out-dir titlized
+
+    or::
+
+        python clean.py --pool True --in-dir split_data_ml/unsplit --out-dir titlized
+    """
     parser = argparse.ArgumentParser(*args)
-    parser.add_argument("in_dir", help="Data source directory")
-    parser.add_argument("out_dir", help="Data output directory")
+    parser.add_argument("--in-dir",
+                        default=os.path.join("split_data_ml", "unsplit"),
+                        help="Data source directory")
+    parser.add_argument("--out-dir", default="clean_data",
+                        help="Data output directory")
+    parser.add_argument("--pool", default=False)
     args = parser.parse_args()
 
-    in_dir = args.in_dir or 'dirty_data'
-    out_dir= args.out_dir or 'clean_data'
-    run(in_dir, out_dir)
+    run(args.in_dir, args.out_dir, args.pool)
 
 
 if __name__ == "__main__":
